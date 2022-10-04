cb4fc0632bd6f2d49c89e428c7af5cdebda943a3
91994 <suddala.rajukumar123@gmail.com>
2022-10-04 22:33:07 +0530

    updating

--- instance.tf
+++ instance.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "testinstance" {
   subnet_id = aws_subnet.subnet[0].id
   associate_public_ip_address= true
   vpc_security_group_ids = [ aws_security_group.ec2.id ]
-  key_name="zwaw.pem"
+  key_name="zwaw"
   tags= {
     Name = "efs-raju"
   }
